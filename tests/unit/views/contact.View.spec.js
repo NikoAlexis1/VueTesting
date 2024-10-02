bb08cc48ac0322a27522a6b462c9c53feeadf791
Nicolas <ncorantehuenchuman@gmail.com>
2024-10-02 15:37:17 -0300

    modificaion y testing de vista contactView.spec.js

--- tests/unit/views/contact.View.spec.js
+++ tests/unit/views/contact.View.spec.js
@@ -1,9 +1,25 @@
-import { shallowMount } from '@vue/test-utils'
 import ContactView from '@/views/ContactView.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import { mount } from '@vue/test-utils'
 
-describe('ContactView', () => {
-    test('Validación de match con el snapshot', () => {
-        const wrapper = shallowMount(ContactView)
-        expect(wrapper.html()).toMatchSnapshot()
-    })
-})
+describe('ContactView', () => { 
+    test('Probando la existencia del componente o vista ContactView ', async () => {
+        const router = createRouter({
+            history: createWebHistory(),
+            routes: [{
+                path: '/contact',
+                name: 'contact',
+                component: ContactView
+            }],
+        })
+        router.push('/contact')
+        await router.isReady()
+
+        const wrapper = mount(ContactView, {
+            global: {
+                plugins: [router]
+            }
+        })
+        expect(wrapper.findComponent(ContactView).exists()).toBe(true)
+    })  
+ })
